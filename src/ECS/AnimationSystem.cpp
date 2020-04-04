9aa44d2e955df2967cd00ec712b12497338770ea
Anton Horbach <antos.horbach@gmail.com>
2020-04-04 15:06:23 +0300

    refactored AnimationSystem

--- src/ECS/AnimationSystem.cpp
+++ src/ECS/AnimationSystem.cpp
@@ -17,14 +17,11 @@ void AnimationSystem::update() {
                 {
                     sc.current_animation = &sc.animations[AnimationInd::IDLE];
                     sc.current_animation->beginTime = SDL_GetTicks();
-                    sc.src.x = 0;
                 }
 
-                if(tc.velocity.x != 0.f) {
-                    if(tc.velocity.y == 0.f
-                        && sc.current_animation != &sc.animations[AnimationInd::WALK])
-                    {
-                        sc.current_animation = &sc.animations[AnimationInd::WALK];
+                if(ac && ac->attacking) {
+                    if(sc.current_animation != &sc.animations[ac->current_attack->animation]) {
+                        sc.current_animation = &sc.animations[ac->current_attack->animation];
                         sc.current_animation->beginTime = SDL_GetTicks();
                     }
                 }
@@ -34,13 +31,14 @@ void AnimationSystem::update() {
                         sc.current_animation->beginTime = SDL_GetTicks();
                     }
                 }
-                else if(ac && ac->attacking) {
-                    if(sc.current_animation != &sc.animations[ac->current_attack->animation]) {
-                        sc.current_animation = &sc.animations[ac->current_attack->animation];
+                else if(tc.velocity.x != 0.f) {
+                    if(sc.current_animation != &sc.animations[AnimationInd::WALK])
+                    {
+                        sc.current_animation = &sc.animations[AnimationInd::WALK];
                         sc.current_animation->beginTime = SDL_GetTicks();
                     }
                 }
-                else if(sc.current_animation != &sc.animations[AnimationInd::IDLE]) {
+                else if(sc.current_animation != &sc.animations[AnimationInd::IDLE]){
                     sc.current_animation = &sc.animations[AnimationInd::IDLE];
                     sc.current_animation->beginTime = SDL_GetTicks();
                 }
@@ -56,18 +54,15 @@ void AnimationSystem::update() {
                     sc.current_animation = &sc.animations[AnimationInd::IDLE];
                     anim = sc.current_animation;
                     anim->beginTime = SDL_GetTicks();
-                    sc.src.x = 0;
                 }
                 
                 sc.src.y = anim->ind * sc.src.h;
-                sc.src.x = sc.src.w * static_cast<int>(
+                sc.src.x = sc.src.w * (anim->offset + static_cast<int>(
                         ((SDL_GetTicks() - anim->beginTime) / anim->speed) % anim->frames
-                );
+                ));
 
-                if(sc.src.x == (sc.src.w * (anim->frames - 1)))
-                {
+                if(sc.src.x == (sc.src.w * (anim->frames - 1))) {
                     anim->played = true;
-                    sc.current_animation->beginTime = 0;
 
                     if(ac) {
                         ac->attacking = false;
