12b4a3522b642a1042e0b53ddf94464d59d370b6
Anton Horbach <antos.horbach@gmail.com>
2020-04-04 11:59:51 +0300

    rewrited vec2 struct

--- src/math/vec2.hpp
+++ src/math/vec2.hpp
@@ -1,15 +1,41 @@
 #ifndef VEC2_HPP
 #define VEC2_HPP
 
-struct vec2f {
-    float x;
-    float y;
+template <typename T>
+struct vec2;
 
-    vec2f& add(const vec2f& oth);
-    vec2f& sub(const vec2f& oth);
+using vec2f = vec2<float>;
 
-    friend vec2f operator+(vec2f left, const vec2f& right);
-    friend vec2f operator-(vec2f left, const vec2f& right);
+template <typename T>
+struct vec2 {
+    T x;
+    T y;
+
+    vec2& operator+=(const vec2& oth) {
+        x += oth.x;
+        y += oth.y;
+
+        return *this;
+    }
+
+    vec2& operator-=(const vec2& oth) {
+        x -= oth.x;
+        y -= oth.y;
+
+        return *this;
+    }
+
+    friend vec2 operator+(const vec2& left, const vec2& right) {
+        vec2 res = left;
+        res += right;
+        return res;
+    }
+
+    friend vec2 operator-(const vec2& left, const vec2& right) {
+        vec2 res = left;
+        res -= right;
+        return res;
+    }
 };
 
 #endif
